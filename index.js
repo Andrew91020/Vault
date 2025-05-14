20090aa70417ef391d9d6b5c43c232b8c1f4a6ad
Andrew Satkiewicz <andrew.satkiewicz@icloud.com>
2025-05-14 13:28:27 -0500

    Change1

---
+++ index.js
@@ -0,0 +1,10 @@
+let code1 = 7 + 3;
+let code2 = 88 / 2 - 4;
+let code3 = (13 % 5) * 13;
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+let message = "The vault has been secured. The combination is:";
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
